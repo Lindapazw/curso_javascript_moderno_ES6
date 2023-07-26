f1e088753f89bc308af1f556287cfbc2e79f6b1a
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-26 11:20:26 -0300

    add string

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,7 @@
+const producto = "Monitor 20 pulgadas";
+const producto2 = String('Monitor');
+const producto3 = new String('Monitor nuevo ');
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
