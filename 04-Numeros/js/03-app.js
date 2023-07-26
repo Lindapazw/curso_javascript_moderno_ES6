bbf4daecac2f631df8b8e48019d5ef4c9a78ec2b
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-26 15:09:24 -0300

    add operaciones con Number

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,37 @@
+let resultado;
+
+// PI
+resultado = Math.PI;
+console.log(resultado);
+
+// Round
+resultado = Math.round(2.7);
+console.log(resultado);
+
+// redondear hacia arriba
+resultado = Math.ceil(2.2);
+console.log(resultado);
+
+// redondear hacia abajo
+resultado = Math.floor(2.9);
+console.log(resultado);
+
+// Valor absoluto
+resultado = Math.abs(-500)
+console.log(resultado);
+
+// Minimo
+resultado = Math.min(2,3)
+console.log(resultado);
+
+// Maximo
+resultado = Math.max(2,3)
+console.log(resultado);
+
+// random
+resultado = Math.random()
+console.log(resultado);
+
+// Floor + random
+resultado = Math.floor(Math.random() * 20)
+console.log(resultado);
