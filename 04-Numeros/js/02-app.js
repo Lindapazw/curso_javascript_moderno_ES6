b68990c859b2eeb814fdc928656a3a24f46213ee
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-26 14:46:51 -0300

    add operation whit Number

---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,24 @@
+const numero1 = 30;
+const numero2 = 10;
+
+let resultado;
+
+// suma
+resultado = numero1 + numero2;
+console.log(resultado);
+
+// resta
+resultado = numero1 - numero2;
+console.log(resultado);
+
+// multiplicacion
+resultado = numero1 * numero2;
+console.log(resultado);
+
+// division
+resultado = numero1 / numero2;
+console.log(resultado);
+
+// modulo
+resultado = numero1 % numero2;
+console.log(resultado);
