05524fbcd3a047064c7c8988a0bd0f93d3bbe1a4
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-26 15:24:52 -0300

    add parseInt and parseFloat

---
+++ 04-Numeros/js/06-app.js
@@ -0,0 +1,10 @@
+const n1 = '20';
+const n2 = '20.2';
+const n3 = 'Uno';
+const n4 = 20;
+
+console.log(n1);
+console.log(Number.parseInt(n1));
+console.log(Number.parseFloat(n2));
+
+console.log(Number.isInteger(n3));
