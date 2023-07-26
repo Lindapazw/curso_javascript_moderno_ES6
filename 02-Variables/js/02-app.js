88a75ba8248418cdc9ed3c32052646f8f8c66594
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-26 11:06:11 -0300

    add  let

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,14 @@
+let producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+//reasingnar valor
+producto = "Monitor de 12 pulgadas";
+console.log(producto);
+
+//reasingnar valor
+producto = "Monitor de 1 pulgadas";
+console.log(producto);
+
+//reasingnar valor
+producto = true;
+console.log(producto);
