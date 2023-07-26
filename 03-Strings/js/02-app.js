f432718bff199ed9e8067c30dd0ea70eefbeb671
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-26 11:23:33 -0300

    add indexOf

--- 03-Strings/js/02-app.js
+++ 03-Strings/js/02-app.js
@@ -3,3 +3,8 @@ console.log(producto);
 
 // metodos en los strings
 console.log(producto.length);
+
+// index of
+console.log(producto.indexOf('Monitor'));
+console.log(producto.indexOf('20'));
+console.log(producto.indexOf('pulgadas'));
