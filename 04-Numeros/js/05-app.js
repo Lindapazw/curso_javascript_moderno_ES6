7b8905d2290915d3362a8c74a09748f94e59b1ae
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-26 15:19:54 -0300

    add incremento

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,13 @@
+let puntaje = 5;
+puntaje++; // 5
+++puntaje; //6
+console.log(puntaje);
+
+let puntaje2 = 5;
+puntaje2--; // 5
+--puntaje2; // 4
+console.log(puntaje2);
+
+let puntaje3 = 5;
+puntaje += 2;
+console.log(puntaje3);
