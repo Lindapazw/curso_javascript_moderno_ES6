f16eb3710de9293b0294430e7224f0ab11d40528
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-26 10:31:15 -0300

    fix error in app.js

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -2,4 +2,4 @@
 const nombre = 'Linda';
 
 // la mostramos en el html
-document.querySelector('.linda').innerHTML = `Nombre: ${nombre}`;0
+document.querySelector('.linda').innerHTML = `Nombre: ${nombre}`; 
