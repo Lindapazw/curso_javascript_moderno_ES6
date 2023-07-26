2d24617660c23c16dc6582bbd68849c13fd9612d
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-26 14:44:04 -0300

    add Number

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,11 @@
+// creación de números 
+const numero1 = 30;
+const numero2 = 20.2;
+const numero3 = .2;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+
+const numero4 = new Number(20);
+console.log(numero4);
