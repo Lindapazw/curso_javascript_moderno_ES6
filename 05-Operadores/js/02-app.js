2c96d8c4a6d7f0238402711f34457a8ce4c218e6
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-28 15:34:12 -0300

    add == and strictic ===

---
+++ 05-Operadores/js/02-app.js
@@ -0,0 +1,16 @@
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = 30;
+
+console.log(numero1 == numero3);
+console.log(numero1 == numero2);
+
+// comparador estricto
+console.log(numero1 === numero2);
+console.log(numero1 === parseInt(numero2));
+
+// diferente
+const password1 = "admin";
+const password2 = "admin";
+
+console.warn(password1 != password2);
