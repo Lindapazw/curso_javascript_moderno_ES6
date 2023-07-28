a7d5640175c6d74f01a79063ec25e4bfd30f9bcf
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-28 15:29:13 -0300

    add operator > <

---
+++ 05-Operadores/js/01-app.js
@@ -0,0 +1,9 @@
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = 30;
+
+// operador mayor a ...
+console.log(numero1 > numero3);
+
+// operador menor a ...
+console.log(numero1 < numero3);
