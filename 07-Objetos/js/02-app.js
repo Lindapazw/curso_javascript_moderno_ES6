93eec65e34fa5ad8a31375b9334478e3447571db
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-30 19:36:21 -0300

    add acceder a valores de un objeto

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,11 @@
+//object literal
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+}
+
+console.log(producto.nombre);
+console.log(producto.precio);
+
+console.log(producto["disponible"]);
