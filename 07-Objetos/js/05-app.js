7d82881de9da3a6226b6471ee59a59575c6e3421
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-30 20:03:16 -0300

    add objetos dentro de objetos

---
+++ 07-Objetos/js/05-app.js
@@ -0,0 +1,14 @@
+//object literal
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+    vivo: true,
+    informacion : {
+        peso: "1kl",
+        medida: "2 metro"
+    }
+}
+
+console.log(producto);
+console.log(producto.informacion); 
