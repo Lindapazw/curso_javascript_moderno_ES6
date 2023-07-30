89f95db0e956a91f7ba7f7ba6d65c58f18be8fbe
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-30 19:48:59 -0300

    add eliminar y agregar nuevos atributos

---
+++ 07-Objetos/js/03-app.js
@@ -0,0 +1,14 @@
+//object literal
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+    vivo: true,
+}
+
+
+// agregar nuevas propiedades al objeto
+producto.imagen = "linda.jpg";
+delete producto.nombre;
+
+console.log(producto);
