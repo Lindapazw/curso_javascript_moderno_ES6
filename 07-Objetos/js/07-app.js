7b8f66a986ca5cadcb2ba931bba45d918ded4f3a
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-30 20:12:43 -0300

    add problema con los objetos

---
+++ 07-Objetos/js/07-app.js
@@ -0,0 +1,11 @@
+//object literal
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+}
+
+const nombreProducto = "Monitor";
+producto.nombre = "Monitor";
+
+console.log(producto);
