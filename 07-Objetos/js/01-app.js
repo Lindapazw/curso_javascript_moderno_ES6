eae14662226e5fd41b569d8458b647c3977795d9
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-30 19:33:33 -0300

    add object literal

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,12 @@
+const nombre = "Monitor 20 pulgadas";
+precio = 300;
+disponible = true;
+
+//object literal
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+}
+
+console.log(producto);
