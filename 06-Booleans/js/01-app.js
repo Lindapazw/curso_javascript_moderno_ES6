4801af51ff60369ff74199e2829b748050f4a1c2
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-30 19:22:40 -0300

    add boolean

---
+++ 06-Booleans/js/01-app.js
@@ -0,0 +1,10 @@
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "false";
+
+console.log(boolean1);
+console.log(boolean2);
+
+console.log(typeof boolean2);
+
+console.log(boolean2 == boolean3);
