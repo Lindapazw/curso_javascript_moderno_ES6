f302f0c00ebd515fe3927d9c86265efc1679f65f
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-30 20:48:19 -0300

    add spread operator

--- 07-Objetos/js/10-app.js
+++ 07-Objetos/js/10-app.js
@@ -14,5 +14,8 @@ console.log(medidas);
 
 const resultado = Object.assign(producto, medidas);
 
+// spread operator o rest operator
+const resultado2 = { ...producto, ...medidas}
+
 console.log(resultado);
 console.log(resultado2);
