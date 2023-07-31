e3d2463134524663aa5395897288a329db3f8840
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-30 21:39:27 -0300

    add object constructor

---
+++ 07-Objetos/js/12-app.js
@@ -0,0 +1,12 @@
+// object constructor
+function Producto(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.disponible = true;
+}
+
+const producto2 = new Producto('Monitor 24 ', 400);
+console.log(producto2);
+
+const producto3 = new Producto('Monitor 30 ', 400);
+console.log(producto3);
