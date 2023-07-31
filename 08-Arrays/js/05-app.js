cbde27920a6413eae9c70e904b9c51754b95ca87
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-31 10:58:15 -0300

    add push and unshift

---
+++ 08-Arrays/js/05-app.js
@@ -0,0 +1,25 @@
+const carrito = [];
+
+// definir un producto 
+const producto = {
+    nombre: 'Monitor 24 pulgadas',
+    precio: 400,
+}
+
+const producto2 = {
+    nombre: 'Play 5',
+    precio: 800,
+}
+
+carrito.push(producto);
+carrito.push(producto2);
+
+const producto3 = {
+    nombre: 'Play 4',
+    precio: 700,
+}
+
+carrito.unshift(producto3);
+
+
+console.table(carrito);
