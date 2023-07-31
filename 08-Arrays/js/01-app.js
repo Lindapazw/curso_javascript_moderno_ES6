733a23134b4e03877946fe9a46b0c8e301d60b7b
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-31 10:38:31 -0300

    add arrays

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3, 4, 5];
+console.log(numeros);
+
+const meses = new Array('1', '2', '3', '4', '5');
+console.log(meses);
+
+// todo tipo de datos
+const deTodo = [1, 'hola', true, false];
+console.log(deTodo);
