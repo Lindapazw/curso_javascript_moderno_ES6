74c15691704be30b148c6b988f612496864d8c41
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-31 15:57:54 -0300

    add metodos in arrow function

---
+++ 09-Funciones/js/11-app.js
@@ -0,0 +1,7 @@
+const aprendiendo2 = (tecnología) => {
+    console.log(`Aprendiendo ${tecnología}`);
+}
+aprendiendo2('JS');
+
+const aprendiendo3 = (tecnología, tecnología2) => `Aprendiendo ${tecnología} y ${tecnología2}`;
+console.log(aprendiendo3('Angular', 'Node'));
