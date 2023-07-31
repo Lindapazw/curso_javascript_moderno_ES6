205fe2a20dd3a5507a0a7136a9299ddd4cc8afb4
Linda Paz <lindacsndelapaz@gmail.com>
2023-07-30 21:56:25 -0300

    add keys / keys / entries

---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,9 @@
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: true,
+}
+
+console.log(Object.keys(producto));
+console.log(Object.values(producto));
+console.log(Object.entries(producto));
