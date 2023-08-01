92fb73327d93f951dfe162e9c8ebf4630f43162e
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-01 09:52:09 -0300

    add set and get

--- 09-Funciones/js/13-app.js
+++ 09-Funciones/js/13-app.js
@@ -1,11 +1,24 @@
 const reproductor = {
-    reproducir: (id) => console.log(`Reproducioendo cancion con el id: ${id}`),
+    cancion: '',
+    reproducir: id => console.log(`Reproducioendo cancion con el id: ${id}`),
     pausar: () => console.log(`Pausando...`),
     borrar : () => console.log('Borrando cancion ...'),
-    crearPlayList : (nombre) => console.log(`crear Play List ${nombre}`),
+    crearPlayList : nombre => console.log(`crear Play List ${nombre}`),
     borrar : () => console.log('Borrando cancion ...'),
-    reproducirPlayList : (nombre) => console.log(`Reproducioendo la playlist con el nombre: ${nombre}`)
+    reproducirPlayList : nombre => console.log(`Reproducioendo la playlist con el nombre: ${nombre}`),
+
+    set nuevaCancion(cancion){
+        this.cancion = cancion;
+        console.log(`Añadiendo ${cancion}`);
+    },
+
+    get obtenerCancion(){
+        console.log(`${this.cancion}`);
+    }
 };
+
+reproductor.nuevaCancion = 'Enter Sandman';
+reproductor.obtenerCancion;
 
 reproductor.crearPlayList('Rock 90');
 reproductor.reproducirPlayList('Rock 90');
