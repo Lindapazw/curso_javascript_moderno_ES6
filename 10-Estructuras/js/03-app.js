ba57473e25d16e560414ea5c136e9a358a441057
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-01 10:39:22 -0300

    add >= and  <=

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,16 @@
+const dinero = 200;
+const totalApagar= 300;
+
+// mayor
+if ( dinero >= totalApagar ) {
+    console.log('Puedes pagar');
+} else {
+    console.log('No puedes pagar');
+}
+
+// menor
+if ( totalApagar <= dinero ) {
+    console.log('Puedes pagar');
+} else {
+    console.log('No puedes pagar');
+}
