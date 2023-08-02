e63cf9fed9296ad147e40e95ecad9e0aa888e0cf
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-02 15:33:22 -0300

    add array + for + .nombre

--- 11-Iteradores/js/01-app.js
+++ 11-Iteradores/js/01-app.js
@@ -6,3 +6,15 @@ for( let i = 0; i < 10; i++ ) {
     }
 }
 
+const carrito = [
+    {nombre: 'Monitor 27 pulgadas', precio: 500},
+    {nombre: 'TV', precio: 100},
+    {nombre: 'Tablet', precio: 200},
+    {nombre: 'Play5 ', precio: 300}
+];
+
+console.log(carrito.length);
+
+for (let i = 0; i < carrito.length; i++) {
+    console.log(carrito[i].nombre);
+}
