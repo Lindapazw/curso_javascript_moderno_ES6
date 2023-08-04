100f29718047bdecf555d0be24e2dec379454f33
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-04 14:13:59 -0300

    add getElementsByClassName

---
+++ 13-DOM/js/02-scripts.js
@@ -0,0 +1,16 @@
+// seleccionar elementos por su clase
+
+const header = document.getElementsByClassName('header');
+// console.log(header);
+
+const hero = document.getElementsByClassName('hero');
+// console.log(hero);
+
+
+// si las clases existen mas de una vez
+const contenedores = document.getElementsByClassName('contenedor');
+console.log(contenedores);
+
+// si no existe
+const noExiste = document.getElementsByClassName('no-existe');
+console.log(noExiste);
