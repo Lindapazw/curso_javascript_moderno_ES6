d53557d4d41ef425d172852bba7f6d345b9f7ba7
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-04 12:11:11 -0300

    add HTML document

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,26 @@
+let elemento;
+// documento
+elemento = document;
+elemento = document.all;
+elemento = document.head;
+elemento = document.body;
+elemento = document.domain;
+elemento = document.forms;
+elemento = document.forms[0];
+elemento = document.forms[0].id;
+elemento = document.forms[0].method;
+elemento = document.forms[0].classList;
+elemento = document.forms[0].action;
+// botones
+elemento = document.links;
+elemento = document.links[4];
+elemento = document.links[4].classList;
+elemento = document.links[4].className;
+// imagenes
+elemento = document.images;
+// scripts
+elemento = document.scripts;
+
+
+
+console.log(elemento);
