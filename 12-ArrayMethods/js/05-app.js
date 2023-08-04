9e04c8825f9c5129891fdbca186f931123979744
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-04 11:45:01 -0300

    add find product with forEach

---
+++ 12-ArrayMethods/js/05-app.js
@@ -0,0 +1,20 @@
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+// con un forEach
+let resultado = '';
+carrito.forEach( (producto, i) => {
+    if(producto.nombre === 'Tablet') {
+        resultado = carrito[i];
+    }
+});
+
+console.log(resultado);
+
+// .find
