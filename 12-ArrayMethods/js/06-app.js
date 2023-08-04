7a42b3bcd733847cf775f3d37b4de0126dcddd69
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-04 11:52:02 -0300

    add .every

---
+++ 12-ArrayMethods/js/06-app.js
@@ -0,0 +1,11 @@
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Tablet', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+const resultado = carrito.every( producto => producto.precio < 1000);
+console.log(resultado);
