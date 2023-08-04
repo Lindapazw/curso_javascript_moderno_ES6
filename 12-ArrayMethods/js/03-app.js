efd15c726d48db924bae9cd88caa7e9a49422af6
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-04 11:34:02 -0300

    add forEach

---
+++ 12-ArrayMethods/js/03-app.js
@@ -0,0 +1,14 @@
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+// con forEach
+let total = 0;
+
+carrito.forEach( producto => total += producto.precio );
+console.log(total);
