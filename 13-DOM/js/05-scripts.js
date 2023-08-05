415ac01ce0d7f806dd65536187665f9c9a1064f3
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-04 21:20:12 -0300

    add querySelectorAll

---
+++ 13-DOM/js/05-scripts.js
@@ -0,0 +1,9 @@
+const card2 = document.querySelectorAll('.card');
+console.log(card2);
+
+const form = document.querySelectorAll('#formulario');
+console.log(form);
+
+// si no existe
+const no = document.querySelectorAll('#no-existe');
+console.log(no);
