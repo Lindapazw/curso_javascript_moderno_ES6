76eb1bbc741d46810d52e9877349ad05b04b98d4
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-05 12:55:11 -0300

    add nodeName and nodeType

--- 13-DOM/js/08-scripts.js
+++ 13-DOM/js/08-scripts.js
@@ -1,4 +1,10 @@
 const navegacion = document.querySelector('.navegacion');
-console.log(navegacion);
-console.log(navegacion.childNodes);
+
+// console.log(navegacion);
+// console.log(navegacion.childNodes);
+console.log(navegacion.children);
 console.log(navegacion.children[0]);
+console.log(navegacion.children[1].nodeName);
+console.log(navegacion.children[1].nodeType);
+
+
