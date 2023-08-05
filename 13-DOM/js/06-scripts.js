ca6490bc8cd707d455e38438d0a9cc0bcc50a621
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-04 21:45:09 -0300

    add imagen.src

--- 13-DOM/js/06-scripts.js
+++ 13-DOM/js/06-scripts.js
@@ -1,6 +1,7 @@
 const titulo = document.querySelector(".contenido-hero h1").textContent = 'Hola';
 console.log(titulo);
 
-
+const imagen = document.querySelector('.card img');
+imagen.src = 'img/hacer2.jpg'
 
 
