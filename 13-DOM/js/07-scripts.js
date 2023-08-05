065d277effa6ab8df9378f52741a963fc6af1f9b
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-04 21:52:58 -0300

    add classList.add and classList.remove

---
+++ 13-DOM/js/07-scripts.js
@@ -0,0 +1,11 @@
+// const encabezado = document.querySelector('h1');
+// console.log(encabezado);
+
+// encabezado.style.backgroundColor = 'red';
+// encabezado.style.fontFamily = 'Arial';
+// encabezado.style.textTransform = 'UpperCase';
+
+const card = document.querySelector('.card');
+card.classList.add('nueva-clase');
+card.classList.remove('card');
+console.log(card.classList);
