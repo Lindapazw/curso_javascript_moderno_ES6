76ca79e95d66755648d227fa246578465f281c23
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-05 13:01:46 -0300

    add .src

--- 13-DOM/js/08-scripts.js
+++ 13-DOM/js/08-scripts.js
@@ -2,9 +2,13 @@ const navegacion = document.querySelector('.navegacion');
 
 // console.log(navegacion);
 // console.log(navegacion.childNodes);
-console.log(navegacion.children);
-console.log(navegacion.children[0]);
-console.log(navegacion.children[1].nodeName);
-console.log(navegacion.children[1].nodeType);
+// console.log(navegacion.children);
+// console.log(navegacion.children[0]);
+// console.log(navegacion.children[1].nodeName);
+// console.log(navegacion.children[1].nodeType);
+
+const card = document.querySelector('.card');
+card.children[0].src = 'img/hacer2.jpg';
+
 
 
