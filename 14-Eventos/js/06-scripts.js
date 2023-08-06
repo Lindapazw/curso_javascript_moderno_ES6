6dc09c506ea2cb664c586fc847da5168aac13ed4
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-06 13:21:51 -0300

    add Event bubbling

---
+++ 14-Eventos/js/06-scripts.js
@@ -0,0 +1,20 @@
+// Event bubbling
+
+const cardDiv = document.querySelector('.card');
+const infoDiv = document.querySelector('.info');
+const titulo = document.querySelector('.titulo');
+
+cardDiv.addEventListener('click', (e) => {
+    
+    console.log('click card');
+});
+
+infoDiv.addEventListener('click', (e) => {
+    e.stopPropagation();
+    console.log('click info');
+});
+
+titulo.addEventListener('click', (e) => {
+    e.stopPropagation();
+    console.log('click titulo');
+});
