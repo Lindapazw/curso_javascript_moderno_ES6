da87d8bfd0153a93cdb35e69454d6ce92f546d6d
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-06 11:19:42 -0300

    add enlace.href

--- 13-DOM/js/10-scripts.js
+++ 13-DOM/js/10-scripts.js
@@ -1,5 +1,9 @@
 const enlace = document.createElement('a');
 
+// Agregando el texto
 enlace.textContent = 'Nuevo enlaceeeee';
+
+// Añadiendo href
+enlace.href = '/nuevo-enlace';
 
 console.log(enlace);
