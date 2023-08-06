fd662d354eea17bbd106dd6ca90558efecdb6e2a
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-06 12:28:36 -0300

    add eventos con teclado

---
+++ 14-Eventos/js/03-scripts.js
@@ -0,0 +1,29 @@
+const busqueda = document.querySelector('.busqueda');
+
+busqueda.addEventListener('keydown', () => {
+    console.log('escribiendo...');
+});
+
+busqueda.addEventListener('keyup', () => {
+    console.log('escribiendo...');
+});
+
+busqueda.addEventListener('blur', () => {
+    console.log('escribiendo...');
+});
+
+busqueda.addEventListener('copy', () => {
+    console.log('escribiendo...');
+});
+
+busqueda.addEventListener('paste', () => {
+    console.log('escribiendo...');
+});
+
+busqueda.addEventListener('cut', () => {
+    console.log('escribiendo...');
+});
+
+busqueda.addEventListener('input', () => {
+    console.log('escribiendo...');
+});
