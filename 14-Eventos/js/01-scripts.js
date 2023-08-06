9350ebdad1802c31daaf6f23f10b63995d0f03ee
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-06 12:13:52 -0300

    add loaded

--- 14-Eventos/js/01-scripts.js
+++ 14-Eventos/js/01-scripts.js
@@ -1,4 +1,9 @@
 // se ejecuta cuando es cargado el html
+console.log(1);
+
 document.addEventListener('DOMContentLoaded',  () => {
     console.log('Document loaded');
+    console.log(2);
 })
+
+console.log(3);
