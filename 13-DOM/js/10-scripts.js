bee70feffc133e01f8830df1090b24b1c66fe4f3
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-06 11:44:04 -0300

    add new card

--- 13-DOM/js/10-scripts.js
+++ 13-DOM/js/10-scripts.js
@@ -38,3 +38,30 @@ console.log(parr1);
 console.log(parr2);
 console.log(parr3);
 
+// insertar en html
+const info = document.createElement('div');
+info.classList.add('info');
+info.appendChild(parr1);
+info.appendChild(parr2);
+info.appendChild(parr3);
+
+// crear imagen
+const img = document.createElement('img');
+img.src = 'img/hacer2.jpg';
+img.alt = 'img';
+
+// crear card
+const card = document.createElement('div');
+card.classList.add('card');
+
+// asignar la imagen
+card.appendChild(img);
+
+// asignar la info
+card.appendChild(info);
+
+console.log(card);
+
+
+
+
