ae927f7578d264c44383387103f8ee65628718f7
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-06 12:01:06 -0300

    add btn whit event listener

--- 13-DOM/js/11-scripts.js
+++ 13-DOM/js/11-scripts.js
@@ -6,7 +6,13 @@ btnFlotante.addEventListener('click', mostrarOcultarFunction);
 function mostrarOcultarFunction() {
     if(footer.classList.contains('activo')) {
         footer.classList.remove('activo');
+        this.classList.remove('activo');
+        this.textContent = 'Idioma y moneda';
+
+
     } else {
         footer.classList.add('activo');
+        this.classList.add('activo');
+        this.textContent = 'X Cerrar';
     }
 }
