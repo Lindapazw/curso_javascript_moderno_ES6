a83eee129cde50f71825fa5053e41cdf0a23baa6
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-07 14:08:53 -0300

    add .eslintrc.json

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,5 +1,7 @@
 // creamos la constante
 const nombre = 'Linda';
+const nombre2 = 'Linda 2';
+const nombre3 = 'Linda 3';
 
 // la mostramos en el html
 document.querySelector('.linda').innerHTML = `Nombre: ${nombre}`;
