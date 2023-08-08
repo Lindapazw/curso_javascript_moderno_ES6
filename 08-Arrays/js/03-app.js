c8a5b06f5cf112125bb99fd6f5672d943cf7bd3e
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-08 17:10:05 -0300

    add recorrer un arreglo

--- 08-Arrays/js/03-app.js
+++ 08-Arrays/js/03-app.js
@@ -1,8 +1,4 @@
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
-console.table(meses);
-
-// cuanto mide el arreglo
-console.log(meses.length);
 
 for (let i = 0; i < meses.length; i++) {
     console.log(meses[i]);
