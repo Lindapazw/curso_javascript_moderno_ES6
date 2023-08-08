c61e822f0c92c398db72597491b3409d82e396a7
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-08 11:16:55 -0300

    add replace, slice, substring

--- 03-Strings/js/05-app.js
+++ 03-Strings/js/05-app.js
@@ -1,17 +1,12 @@
 const producto = "Monitor 20 pulgadas";
 
-
-//replace
-console.log(producto);
-console.log(producto.replace('pulgadas', '"'));
-console.log(producto.replace('Monitor', 'Monitor curvo'));
-
 //slice para cortar 
 console.log(producto.slice(0,10));
-console.log(producto.slice(8));
+console.log(producto.slice(1));
 
 // substring
 console.log(producto.substring(8));
+console.log(producto.substring(2,1));
 
 // Mostramos la primera letra del nombre
 const usuario = "Linda";
@@ -19,3 +14,8 @@ console.log(usuario.substring(0,1));
 
 // chart At
 console.log(usuario.charAt(0));
+
+//replace
+console.log(producto);
+console.log(producto.replace('pulgadas', '"'));
+console.log(producto.replace('Monitor', 'Monitor curvo'));
