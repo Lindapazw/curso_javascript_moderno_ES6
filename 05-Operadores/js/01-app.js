bba0737d6833dd6aa9305e23a94773fe2a28413e
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-08 13:47:05 -0300

    add < >

--- 05-Operadores/js/01-app.js
+++ 05-Operadores/js/01-app.js
@@ -3,7 +3,8 @@ const numero2 = "20";
 const numero3 = 30;
 
 // operador mayor a ...
-console.log(numero1 > numero3);
+console.log(numero1 > numero2);
 
 // operador menor a ...
-console.log(numero1 < numero3);
+console.log(numero1 < numero2);
+
