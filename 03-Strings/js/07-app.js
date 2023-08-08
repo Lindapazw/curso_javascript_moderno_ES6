304be6feb732c9b02cf0b2c407cdc7690d04d1df
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-08 11:40:46 -0300

    add toLowerCase and toLowerCase

--- 03-Strings/js/07-app.js
+++ 03-Strings/js/07-app.js
@@ -1,7 +1,6 @@
 const producto = 'Monitor 20 pulgadas';
-
 console.log(producto.toLowerCase());
-console.log(producto.toUpperCase());
 
 const email = "correo@gmail.com";
 console.log(email.toLowerCase());
+
