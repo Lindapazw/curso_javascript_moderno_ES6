eaf912947babcad1a29263eba451ef3a08c2b78b
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-08 12:14:12 -0300

    add incremento y decremento

--- 04-Numeros/js/05-app.js
+++ 04-Numeros/js/05-app.js
@@ -2,12 +2,15 @@ let puntaje = 5;
 puntaje++; // 5
 ++puntaje; //6
 console.log(puntaje);
+// se tiene que volver a llamar a la variable para que se muestre el cambio
 
 let puntaje2 = 5;
 puntaje2--; // 5
 --puntaje2; // 4
 console.log(puntaje2);
+// se resta uno
 
 let puntaje3 = 5;
 puntaje += 2;
 console.log(puntaje3);
+// se asigna sobre el valor existente
