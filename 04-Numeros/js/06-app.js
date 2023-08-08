c257fa2881ccc0d25bdc61e3a8fc810fb74d2f43
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-08 12:22:04 -0300

    add parse

--- 04-Numeros/js/06-app.js
+++ 04-Numeros/js/06-app.js
@@ -3,8 +3,8 @@ const n2 = '20.2';
 const n3 = 'Uno';
 const n4 = 20;
 
-console.log(n1);
 console.log(Number.parseInt(n1));
 console.log(Number.parseFloat(n2));
-
 console.log(Number.isInteger(n3));
+
+
