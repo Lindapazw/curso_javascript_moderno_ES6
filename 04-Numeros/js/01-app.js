42c2cf1cdc34a2f586e1be78b10d429a35fbadc0
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-08 11:56:17 -0300

    add number

--- 04-Numeros/js/01-app.js
+++ 04-Numeros/js/01-app.js
@@ -2,10 +2,10 @@
 const numero1 = 30;
 const numero2 = 20.2;
 const numero3 = .2;
+const numero4 = new Number(20);
 
 console.log(numero1);
 console.log(numero2);
 console.log(numero3);
-
-const numero4 = new Number(20);
 console.log(numero4);
+
