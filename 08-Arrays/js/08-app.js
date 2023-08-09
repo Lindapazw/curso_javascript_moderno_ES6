88d799b0f74c14c1638e779f47d629ac44320946
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-09 10:12:19 -0300

    add array destructuring

--- 08-Arrays/js/08-app.js
+++ 08-Arrays/js/08-app.js
@@ -1,18 +1,9 @@
-//object literal
-const producto = {
-    nombre: "Monitor 20 pulgadas",
-    precio: 300,
-    disponible: true,
-    vivo: true,
-}
-
-// forma nueva
-const {vivo,disponible} = producto	
-console.log(vivo, disponible);
-
-
 // destructuring con arreglos
 const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+const [uno] = numeros;
+console.log(uno);
+
 const [ , , tres] = numeros;
 console.log(tres);
 
