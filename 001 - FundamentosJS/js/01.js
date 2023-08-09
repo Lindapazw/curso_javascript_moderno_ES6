9be21a90889cbcb7b84898dea1baea86da54fec7
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-09 13:53:21 -0300

    add let

--- 001 - FundamentosJS/js/01.js
+++ 001 - FundamentosJS/js/01.js
@@ -1,1 +1,10 @@
-console.log('Hola mundo');
+let cliente = 'Linda';
+let descuento = 200;
+let precio;
+
+// reasignar
+cliente = 'Juan';
+
+console.log(cliente);
+console.log(descuento);
+console.log(precio);
