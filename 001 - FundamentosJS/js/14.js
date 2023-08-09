c5be9e4efb07f49f2f501f52f7f824bd01a43373
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-09 17:01:18 -0300

    add function declaration vs function expression

---
+++ 001 - FundamentosJS/js/14.js
@@ -0,0 +1,12 @@
+// function declaration
+sumar(10,10)
+function sumar(a,b) {
+    console.log(a+b);
+};
+
+
+// function expression
+sumar2(100,100);
+const sumar2 = function(a,b) {
+    console.log(a+b);
+}
