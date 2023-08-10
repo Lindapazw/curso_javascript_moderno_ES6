d5842c186ce34e56ad9833d7ad5f84d0481a9f1e
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-10 13:45:23 -0300

    add ternario anidado

--- 10-Estructuras/js/09-app.js
+++ 10-Estructuras/js/09-app.js
@@ -4,3 +4,11 @@ const pueedePagar = true;
 console.log(autenticado && pueedePagar? 'si puede': 'no puede');
 console.log(autenticado || pueedePagar? 'si puede': 'no puede');
 
+// otro ejemplo 
+const nota = 65;
+const grado =   nota >= 95? "A+":
+                nota >= 85? "B+": 
+                nota >= 75? "C+":
+                nota >= 65? "D+": "F";
+
+
