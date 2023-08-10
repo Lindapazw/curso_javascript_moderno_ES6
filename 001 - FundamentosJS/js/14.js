e36644c4ef4456c6428ace46a648901ad5bb6b75
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-10 10:47:31 -0300

    add arrow function with param

--- 001 - FundamentosJS/js/14.js
+++ 001 - FundamentosJS/js/14.js
@@ -4,9 +4,20 @@ function sumar(a,b) {
     console.log(a+b);
 };
 
-
 // function expression
-sumar2(100,100);
 const sumar2 = function(a,b) {
     console.log(a+b);
 }
+sumar2(100,100);
+
+// arrow function
+const sumar3 = (a,b) => {
+    console.log(a+b);
+}
+sumar3(100,100);
+
+const tecnologias = (a,b) => {
+    console.log(`Tengo ${a} ${b}`);
+}
+
+tecnologias(100,100); 
