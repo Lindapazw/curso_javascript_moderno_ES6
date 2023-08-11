c8244e8e7476ce275452a7dd698c8ba9c2a1e87c
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-11 12:00:39 -0300

    add nuevo ternario

---
+++ 001 - FundamentosJS/js/16.js
@@ -0,0 +1,11 @@
+const saldo = 300;
+const pagar = 300;
+const tarjeta = 300;
+
+saldo > pagar ? 
+    console.log('Puedes pagar') : 
+    tarjeta? 
+        console.log('Puedes pagar con tarjeta') :
+        console.log('No puedes pagar');
+
+        
