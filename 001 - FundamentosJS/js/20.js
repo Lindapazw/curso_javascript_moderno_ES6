7b5a4dcac67402eba4c32313534b9efe9d233beb
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-11 15:29:48 -0300

    add eventos del DOMcon arrow function

--- 001 - FundamentosJS/js/20.js
+++ 001 - FundamentosJS/js/20.js
@@ -1,8 +1,9 @@
 // eventos del DOM
+const mensaje = () => {
+    console.log('diste click');
+}
+
 const heading = document.querySelector('#heading');
 heading.addEventListener('click', mensaje);
 
-function mensaje(){
-    console.log('Diste click en heading');
-}
 
