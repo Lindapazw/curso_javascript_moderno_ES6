d7c0296c0762b28f6b8e950b7331e2538685af46
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-11 12:26:54 -0300

    add querySelector - querySelectorAll

---
+++ 001 - FundamentosJS/js/18.js
@@ -0,0 +1,21 @@
+// selectores del DOM
+
+const heading = document.querySelector('h2');
+console.log(heading.textContent);
+console.log(heading.tagName);
+console.log(heading.classList);
+
+console.log('----------------------------------------------------------------');
+
+const headingClass = document.querySelector('.heading');
+console.log(headingClass);
+
+const headingId = document.querySelector('#heading');
+console.log(headingId);
+
+
+// querySelectorAll
+
+const enlaces = document.querySelectorAll('a');
+console.log(enlaces);
+
