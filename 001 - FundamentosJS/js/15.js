45e3e905e42a2519ee16920c98629b20de3330a9
Linda Paz <lindacsndelapaz@gmail.com>
2023-08-11 11:18:51 -0300

    add some includes filter

--- 001 - FundamentosJS/js/15.js
+++ 001 - FundamentosJS/js/15.js
@@ -1,7 +1,13 @@
 const tecnologias = ['Angular', 'JavaScript', 'java', 'react', 'Cobol'];
 
+// filtra todos
 const nuevoArray = tecnologias.filter(tech =>  tech !== 'Angular')
 console.log(nuevoArray);
 
+// incluye
 const filtro = tecnologias.includes('react');
 console.log(filtro);
+
+// Al menos uno
+const resultado = tecnologias.some( tech => tech !== 'Angular');
+console.log(resultado);
